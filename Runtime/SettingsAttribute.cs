e2c9f8000b87fc7a0f858cc8aecc76a47ec859d5
krisrok <3404365+krisrok@users.noreply.github.com>
2023-08-07 10:01:00 +0200

    Update license comments

--- Runtime/SettingsAttribute.cs
+++ Runtime/SettingsAttribute.cs
@@ -1,5 +1,6 @@
-// Copyright 2021 by Hextant Studios. https://HextantStudios.com
-// This work is licensed under CC BY 4.0. http://creativecommons.org/licenses/by/4.0/
+﻿// Derivative of "https://github.com/hextantstudios/com.hextantstudios.utilities" 
+//  Original Copyright 2021 by Hextant Studios. https://HextantStudios.com
+//  Used under CC BY 4.0. http://creativecommons.org/licenses/by/4.0/
 using System;
 
 namespace SerializableSettings
