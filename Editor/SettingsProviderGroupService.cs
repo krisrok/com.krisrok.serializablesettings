0899a4b9ba7b06e1bd02b6939055106d406a779c
krisrok <3404365+krisrok@users.noreply.github.com>
2023-09-06 15:11:31 +0200

    Simplify SettingsProviderGroupService by using TypeCache

--- Editor/SettingsProviderGroupService.cs
+++ Editor/SettingsProviderGroupService.cs
@@ -16,127 +16,36 @@ namespace SerializableSettings.Editor
     /// </summary>
     public static class SettingsProviderGroupService
     {
-        private class ScannedAssemblyInfo
-        {
-            public Assembly Assembly;
-            public ScannedTypeInfo[] Types;
-        }
-
-        private class ScannedTypeInfo
-        {
-            public Type Type;
-            public Attribute Attribute;
-            public PropertyInfo InstanceProp;
-            public PropertyInfo DisplayPathProp;
-
-            public ScannedTypeInfo(Type type)
-            {
-                Type = type;
-                Attribute = type.GetCustomAttribute<SettingsAttributeBase>(inherit: true);
-                InstanceProp = type.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
-                DisplayPathProp = type.GetProperty("DisplayPath", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
-            }
-        }
-
-        private static ScannedAssemblyInfo[] _assemblyInfos;
-
-        private static ScannedAssemblyInfo[] GetAssemblyInfos()
-        {
-            if (_assemblyInfos != null)
-                return _assemblyInfos;
-
-#if SETTINGSPROVIDERGROUPSERVICE_DEBUG
-            VerboseLog($"{nameof(SettingsProviderGroupService)}.{nameof(GetAssemblyInfos)}");
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-#endif
-
-            var assemblyInfos = PerformFullScan();
-
-            _assemblyInfos = assemblyInfos.ToArray();
-
-#if SETTINGSPROVIDERGROUPSERVICE_DEBUG
-            VerboseLog($"{nameof(SettingsProviderGroupService)}.{nameof(GetAssemblyInfos)} ran in {stopwatch.Elapsed.TotalSeconds}s");
-#endif
-
-            return _assemblyInfos;
-        }
-
-        private static Assembly[] GetReferencingAssemblies()
-        {
-            var attributeAssemblyName = typeof(SettingsAttributeBase).Assembly.FullName;
-
-            // todo: most likely, we can filter out lots of those
-            return AppDomain.CurrentDomain.GetAssemblies()
-                .Where(a => a.IsDynamic == false)
-                .Where(a => a.GetReferencedAssemblies().Any(ra => ra.FullName == attributeAssemblyName))
-                .ToArray();
-        }
-
-        private static ScannedAssemblyInfo[] PerformFullScan()
-        {
-            VerboseLog("Performing a full scan");
-
-            var referencingAssemblies = GetReferencingAssemblies();
-
-            var scannedAssemblyInfos = referencingAssemblies
-                .Select(a => ScanAssembly(a))
-                .ToArray();
-
-            return scannedAssemblyInfos;
-        }
-
-        private static ScannedAssemblyInfo ScanAssembly(Assembly assembly)
-        {
-            VerboseLog($"Scanning {assembly.FullName}");
-
-            var typeAttributeTuples = assembly.DefinedTypes
-                .Select(t => (Type: t, Attribute: t.GetCustomAttribute<SettingsAttributeBase>(inherit: true)))
-                .Where(ta => ta.Attribute != null);
-
-            var types = new List<ScannedTypeInfo>();
-
-            foreach (var ta in typeAttributeTuples)
-            {
-                if (ta.Type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).Any(mi => mi.GetCustomAttribute<SettingsProviderAttribute>() != null))
-                {
-                    VerboseLog($"Skipping type (has SettingsProvider): {ta.Type}");
-                    continue;
-                }
-
-                if (typeof(ISettingsInternals).IsAssignableFrom(ta.Type) == false)
-                {
-                    Debug.LogError($"{ta.Type} is decorated with {ta.Attribute.GetType()} but does not inherit from Settings<>!\nPlease remove the attribute or fix the inheritance to e.g. Settings<{ta.Type}>.");
-                    continue;
-                }
-
-                VerboseLog($"Adding type: {ta.Type}");
-
-                types.Add(new ScannedTypeInfo(ta.Type));
-            }
-
-            var result = new ScannedAssemblyInfo
-            {
-                Assembly = assembly,
-                Types = types.ToArray()
-            };
-
-            return result;
-        }
-
         [SettingsProviderGroup]
         public static SettingsProvider[] CreateProviders()
         {
-            var assemblyInfos = GetAssemblyInfos();
-
             var result = new List<SettingsProvider>();
 
-            foreach (var at in assemblyInfos.SelectMany(ai => ai.Types))
+            var types = TypeCache.GetTypesWithAttribute<SettingsAttributeBase>();
+            foreach (var type in types)
             {
-                result.Add(new ScriptableObjectSettingsProvider(() => (ScriptableObject)at.InstanceProp.GetValue(null),
-                    at.Attribute is EditorUserSettingsAttribute ?
+                if (type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).Any(mi => mi.GetCustomAttribute<SettingsProviderAttribute>() != null))
+                {
+                    VerboseLog($"Skipping type (has SettingsProvider): {type}");
+                    continue;
+                }
+
+                var attribute = type.GetCustomAttribute<SettingsAttributeBase>(inherit: true);
+                if (typeof(ISettingsInternals).IsAssignableFrom(type) == false)
+                {
+                    Debug.LogError($"{type} is decorated with {attribute.GetType()} but does not inherit from Settings<>!\nPlease remove the attribute or fix the inheritance to e.g. Settings<{type}>.");
+                    continue;
+                }
+
+                var instanceProp = type.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
+                var displayPathProp = type.GetProperty("DisplayPath", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
+                
+                VerboseLog($"Adding type: {type}");
+
+                result.Add(new ScriptableObjectSettingsProvider(() => (ScriptableObject)instanceProp.GetValue(null),
+                    attribute is EditorUserSettingsAttribute ?
                     SettingsScope.User : SettingsScope.Project,
-                    (string)at.DisplayPathProp.GetValue(null)));
+                    (string)displayPathProp.GetValue(null)));
             }
 
             return result.ToArray();
